ba6c162f9b4a3469ffb00d5f58191da1711e23a9
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-15 20:25:27 -0800

    add first business logic fild to be tested

---
+++ CarDealership/Models/Car.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace CarDealership.Models
+{
+  public class Car
+  {
+    public string MakeModel { get; set; }
+    // public int Price;
+    // public int Miles;
+
+  }
+}
